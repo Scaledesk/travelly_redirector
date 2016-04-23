7a5e9cb517f95d85dc259a4fa0ed143994da2181
Ubuntu <ubuntu@ip-172-31-3-87.ap-southeast-1.compute.internal>
2016-04-23 14:08:18 +0000

    Cors config

--- travelly/settings.py
+++ travelly/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -52,6 +54,8 @@ MIDDLEWARE_CLASSES = [
 ]
 
 ROOT_URLCONF = 'travelly.urls'
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
